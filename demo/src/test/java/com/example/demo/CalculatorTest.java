459263dd5ddb75ab15af7281f3e668ce519d7a54
naveendran <sujeenthiran.naveendran@ynov.com>
2024-02-24 15:54:23 +0100

    test

---
+++ demo/src/test/java/com/example/demo/CalculatorTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        // Votre code de test pour la méthode add
+    }
+
+    @Test
+    public void testMulti() {
+        // Votre code de test pour la méthode multi
+    }
+
+    @Test
+    public void testDiv() {
+        // Votre code de test pour la méthode div
+    }
+}
