7e53f927c02062a917bc8d8c34aee044b088c96e
naveendran <sujeenthiran.naveendran@ynov.com>
2024-02-27 08:09:00 +0100

    okoo

--- demo/src/main/java/com/example/demo/Calculator.java
+++ demo/src/main/java/com/example/demo/Calculator.java
@@ -1,17 +1,16 @@
-package com.example.demo;
-
 public class Calculator {
-    public static int add(int a, int b) {
+
+    public static Integer add(Integer a, Integer b) {
         return a + b;
     }
 
-    public static int multi(int a, int b) {
+    public static Integer multi(Integer a, Integer b) {
         return a * b;
     }
 
-    public static int div(int a, int b) {
+    public static Integer div(Integer a, Integer b) {
         if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
+            throw new IllegalArgumentException("Division by zero is not allowed");
         }
         return a / b;
     }
--- demo/src/test/java/com/example/demo/CalculatorTest.java
+++ demo/src/test/java/com/example/demo/CalculatorTest.java
@@ -1,5 +1,3 @@
-package com.example.demo;
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -7,16 +5,16 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        assertEquals(5, Calculator.add(2, 3));
+        assertEquals(Integer.valueOf(5), Calculator.add(2, 3));
     }
 
     @Test
-    public void testMultiply() {
-        assertEquals(6, Calculator.multi(2, 3));
+    public void testMulti() {
+        assertEquals(Integer.valueOf(6), Calculator.multi(2, 3));
     }
 
     @Test
-    public void testDivide() {
-        assertEquals(2, Calculator.div(6, 3));
+    public void testDiv() {
+        assertEquals(Integer.valueOf(2), Calculator.div(6, 3));
     }
 }
