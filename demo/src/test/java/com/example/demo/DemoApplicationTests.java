a27737fd5b41c689276bcf13e14d36b6ab92c34f
naveendran <sujeenthiran.naveendran@ynov.com>
2024-02-27 12:23:52 +0100

    okoo

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,43 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    // Classe Calculatrice définie directement dans le fichier de test
+    static class Calculatrice {
 
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        public int subtract(int a, int b) {
+            return a - b;
+        }
+
+        public int multiply(int a, int b) {
+            return a * b;
+        }
+    }
+
+    @Test
+    public void testAddition() {
+        Calculatrice calculatrice = new Calculatrice();
+        assertThat(calculatrice.add(2, 3)).isEqualTo(5);
+    }
+    
+    @Test
+    public void testSubtraction() {
+        Calculatrice calculatrice = new Calculatrice();
+        assertThat(calculatrice.subtract(5, 3)).isEqualTo(2);
+    }
+
+    
+    @Test
+    public void testMultiplication() {
+        Calculatrice calculatrice = new Calculatrice();
+        assertThat(calculatrice.multiply(2, 3)).isEqualTo(6);
+    }
 }
