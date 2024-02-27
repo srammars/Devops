24ae0f88687788747994193508bbe20ac7488d75
naveendran <sujeenthiran.naveendran@ynov.com>
2024-02-27 14:33:38 +0100

    insta

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,22 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DemoApplicationTests {
-
-    static class Calculatrice {
-
-        public int add(int a, int b) {
-            return a + b;
-        }
-
-        public int subtract(int a, int b) {
-            return a - b;
-        }
-
-        public int multiply(int a, int b) {
-            return a * b;
-        }
-    }
-
     @Test
     public void testAddition() {
         Calculatrice calculatrice = new Calculatrice();
