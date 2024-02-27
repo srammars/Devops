b12ca114c53a03ccf5e43b19b1c2b976c9341d53
naveendran <sujeenthiran.naveendran@ynov.com>
2024-02-27 07:46:46 +0100

    okoo

--- demo/src/test/java/com/example/demo/DummyTest.java
+++ demo/src/test/java/com/example/demo/DummyTest.java
@@ -5,17 +5,20 @@ public class DummyTest {
 
     @Test
     public void testAdd() {
-        assertEquals(4, add(2, 2));
+        DummyTest dummyTest = new DummyTest();
+        assertEquals(4, dummyTest.add(2, 2));
     }
 
     @Test
     public void testMulti() {
-        assertEquals(6, multi(2, 3));
+        DummyTest dummyTest = new DummyTest();
+        assertEquals(6, dummyTest.multi(2, 3));
     }
 
     @Test
     public void testDiv() {
-        assertEquals(2, div(6, 3));
+        DummyTest dummyTest = new DummyTest();
+        assertEquals(2, dummyTest.div(6, 3));
     }
 
     public int add(int a, int b) {
